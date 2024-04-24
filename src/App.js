e21afaaf1e209f71714765162b096fdac3866c6f
HabibiTaha <119021772+HabibiTaha@users.noreply.github.com>
2024-04-23 23:18:56 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from './LoginPage/LoginPage';
-import HeaderArea from './MainArea/HeaderArea';
+import HeaderArea from './HeaderArea/HeaderArea';
 function App() {
   return (
     <div className="App">
