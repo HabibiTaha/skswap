585ebeb2f1994e01c68157e6fbb689bd71e7d13c
HabibiTaha <119021772+HabibiTaha@users.noreply.github.com>
2024-04-23 23:10:18 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import LoginPage from './LoginPage/LoginPage';
+import HeaderArea from './MainArea/HeaderArea';
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/LoginPage' element={<LoginPage />} />
+        <Route path='/HeaderArea' element={<HeaderArea />} />
+        </Routes>
+    </div>
+  );
+}
+
+export default App;
